baf83b6c0fcea3286a226d42403e95079c4c1e95
mayayassur <maya.yassur.42@gmail.com>
2023-01-09 13:25:36 +0200

    absgit

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import numpy as np
 
 
-def flip_spin(i, j, p):
+def flip_spin(spin_lattice, energy_lattice, x, y, p, eta):
     pass
 
 
-def flip_all():
-    pass
+def flip_all(spin_lattice, energy_lattice):
+    rows = len(spin_lattice)
+    cols = len(spin_lattice[0])
+    p = np.random.random((rows, cols))
+    for i in range(rows):
+        for j in range(cols):
+            flip_spin(spin_lattice, energy_lattice, i, j, p[i, j], eta)
 
 
-def stop():
+def stop(f1, f2):
     pass
+
 
 def energy_setup(spin_lattice, energy_lattice):
     """
@@ -32,9 +38,23 @@ def energy_setup(spin_lattice, energy_lattice):
             energy_lattice *= spin_lattice[x, y]
 
 
-def simulate():
+def single_run(spin_lattice, energy_lattice, k):
+    for i in range(k):
+        flip_all(spin_lattice, energy_lattice)
+
+    return M, U
+
+
+def simulate(k):
     spin_lattice = np.ones((lattice_size, lattice_size))
     energy_lattice = np.zeros((lattice_size, lattice_size))
+    energy_setup(spin_lattice, energy_lattice)
+    first = single_run(k)
+    second = single_run(2 * k)
+    while stop(first, second):
+        k = 2 * k
+        first = second
+        second = single_run(2 * k)
 
 
 if __name__ == '__main__':
