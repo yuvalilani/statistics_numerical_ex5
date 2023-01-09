6aa0fd46e8c545f6f1b387acff2917d349de861d
mayayassur <maya.yassur.42@gmail.com>
2023-01-09 13:57:37 +0200

    added full sim

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import numpy as np
 
 
 def p_flip(energy):
+    print(energy)
     return 1 / (np.exp(-2 * energy) + 1)
 
 
-def flip_spin(spin_lattice, energy_lattice, x, y, p, eta):
+def flip_spin(spin_lattice, energy_lattice, lattice_size, x, y, p, eta):
     energy = energy_lattice[x, y]
     if p < p_flip(energy):
         spin_lattice[x, y] *= -1
@@ -18,24 +19,22 @@ def flip_spin(spin_lattice, energy_lattice, x, y, p, eta):
             energy_lattice[x - 1, y] += energy_delta * spin_lattice[x - 1, y]
         if y > 0:
             energy_lattice[x, y - 1] += energy_delta * spin_lattice[x, y - 1]
-    pass
 
 
-def flip_all(spin_lattice, energy_lattice):
-    rows = len(spin_lattice)
-    cols = len(spin_lattice[0])
-    p = np.random.random((rows, cols))
-    for i in range(rows):
-        for j in range(cols):
-            flip_spin(spin_lattice, energy_lattice, i, j, p[i, j], eta)
+def flip_all(spin_lattice, energy_lattice, lattice_size, p, eta):
+    for i in range(lattice_size):
+        for j in range(lattice_size):
+            flip_spin(spin_lattice, energy_lattice, lattice_size, i, j, p[i, j], eta)
 
 
-def dont_stop(m1, m2, k):
+def dont_stop(first, second, k):
+    m1 = first[0]
+    m2 = second[0]
     delta = 10**-3
     return not (abs(m1 - m2) / abs(m2) < delta or k > 10 ** 8)
 
 
-def energy_setup(spin_lattice, energy_lattice, h, eta):
+def energy_setup(spin_lattice, energy_lattice, lattice_size, h, eta):
     """
     CHANGES THE energy_lattice
     :param energy_lattice:
@@ -55,29 +54,47 @@ def energy_setup(spin_lattice, energy_lattice, h, eta):
             energy_lattice *= spin_lattice[x, y]
 
 
-def single_run(spin_lattice, energy_lattice, k):
+def single_run(spin_lattice, energy_lattice, lattice_size, k, n_sweep, h, eta):
+    p = np.random.random((k*n_sweep, lattice_size, lattice_size))
+    M = 0
+    M_sqr = 0
+    U = 0
+    U_sqr = 0
     for i in range(k):
-        flip_all(spin_lattice, energy_lattice)
+        for j in range(n_sweep):
+            flip_all(spin_lattice, energy_lattice, lattice_size, p[i*n_sweep+j], eta)
+        m = np.sum(spin_lattice)
+        u = 2*energy_lattice + m * h
+        M += m
+        M_sqr += m**2
+        U += u
+        U_sqr += u**2
+    M /= k
+    U /= k
+    M_sqr /= k
+    U_sqr /= k
+    return M, M_sqr, U, U_sqr
 
-    return M, U
 
-
-def simulate(k):
+def simulate(k, h, eta):
+    n_sweep = 5
+    lattice_size = 32
     spin_lattice = np.ones((lattice_size, lattice_size))
     energy_lattice = np.zeros((lattice_size, lattice_size))
-    energy_setup(spin_lattice, energy_lattice)
-    first = single_run(k)
-    second = single_run(2 * k)
-    while stop(first, second, ):
+    energy_setup(spin_lattice, energy_lattice, lattice_size, h, eta)
+    first = single_run(spin_lattice, energy_lattice, lattice_size, k, n_sweep, h, eta)
+    second = single_run(spin_lattice, energy_lattice, lattice_size, 2*k, n_sweep, h, eta)
+    while dont_stop(first, second, k):
         k = 2 * k
         first = second
-        second = single_run(2 * k)
+        second = single_run(spin_lattice, energy_lattice, lattice_size, 2*k, n_sweep, h, eta)
+        print(k)
+    return second
 
 
 if __name__ == '__main__':
     # parameters
-    lattice_size = 32
+    k = 25000
     h = 1
     eta = 0.5
-    n_sweep = 5
-    simulate()
+    M, M_sqr, U, U_sqr = simulate(k, h, eta)
