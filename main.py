704b00dccaf3f9780afb0e527b5792e5ef22b181
mayayassur <maya.yassur.42@gmail.com>
2023-01-09 13:04:09 +0200

    abs

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
-if __name__ == '__main__':
-    # parameters
-    lattice_size = 32
-    h = 1
-    eta = 0.5
-    n_sweep = 5
-    spin_lattice = np.ones((lattice_size, lattice_size))
-    energy_lattice = np.zeros((lattice_size, lattice_size))
+
+def flip_spin(i, j, p):
+    pass
+
+
+def flip_all():
+    pass
+
+
+def stop():
+    pass
+
+def energy_setup(spin_lattice, energy_lattice):
+    """
+    CHANGES THE energy_lattice
+    :param energy_lattice:
+    :return:
+    """
     for x in range(lattice_size):
         for y in range(lattice_size):
             if x < lattice_size - 1:
@@ -21,3 +30,17 @@ if __name__ == '__main__':
                 energy_lattice[x, y] -= eta * spin_lattice[x, y - 1]
             energy_lattice -= h
             energy_lattice *= spin_lattice[x, y]
+
+
+def simulate():
+    spin_lattice = np.ones((lattice_size, lattice_size))
+    energy_lattice = np.zeros((lattice_size, lattice_size))
+
+
+if __name__ == '__main__':
+    # parameters
+    lattice_size = 32
+    h = 1
+    eta = 0.5
+    n_sweep = 5
+    simulate()
